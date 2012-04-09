1c72c7547f7b3e021a69a003113baad1df080007
oscar <tuxskar@gmail.com>
2012-04-09 16:41:43 +0200

    Added map view, api-key and MapActivity

--- src/main/java/com/bandoleros/geoalarms/HomeActivity.java
+++ src/main/java/com/bandoleros/geoalarms/HomeActivity.java
@@ -1,15 +1,22 @@
 package com.bandoleros.geoalarms;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class HomeActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class HomeActivity extends Activity {
+	/** Called when the activity is first created. */
+	private final int MAPACTIVITY = 1;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
+	}
+
+	public void toMap(View v) {
+		Intent intent = new Intent(HomeActivity.this,MapActivityView.class);
+		this.startActivityForResult(intent, MAPACTIVITY);
+	}
 }
---
+++ src/main/java/com/bandoleros/geoalarms/MapActivityView.java
@@ -0,0 +1,28 @@
+package com.bandoleros.geoalarms;
+
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+
+
+public class MapActivityView extends MapActivity{
+
+	public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+        
+        MapView map = (MapView) findViewById(R.id.mapView);
+        
+        map.setBuiltInZoomControls(true);
+        
+    }
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
