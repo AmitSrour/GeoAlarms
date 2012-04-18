f2add159c413a32837ce64c476ee6917fbde4e86
oscar <tuxskar@gmail.com>
2012-04-18 19:43:34 +0200

    Fixed the problem with the layout :), it was a import problem (mea culpa)

--- src/main/java/com/bandoleros/geoalarms/activity/Help.java
+++ src/main/java/com/bandoleros/geoalarms/activity/Help.java
@@ -2,9 +2,7 @@ package com.bandoleros.geoalarms.activity;
 
 import com.bandoleros.geoalarms.R;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class Help extends Activity {
 
