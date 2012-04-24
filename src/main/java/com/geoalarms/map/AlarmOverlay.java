6835c869c5f0f90f290daac665fc610975f06f0b
Alejandro Gómez <alejandroogomez@gmail.com>
2012-04-24 14:50:11 +0200

    pretty alarm plotting

--- src/main/java/com/geoalarms/map/AlarmOverlay.java
+++ src/main/java/com/geoalarms/map/AlarmOverlay.java
@@ -1,8 +1,14 @@
 package com.geoalarms.map;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.drawable.Drawable;
 import android.graphics.Point;
 
 import com.google.android.maps.GeoPoint;
@@ -10,34 +16,59 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.Projection;
 
+import com.geoalarms.R;
+import com.geoalarms.GeoAlarms;
 import com.geoalarms.model.Alarm;
 import com.geoalarms.model.Coordinates;
 
 
 public class AlarmOverlay extends Overlay {
+
 	public Alarm alarm;
+	private Bitmap alarmIcon;
 
 	public AlarmOverlay(Alarm alarm) {
 		this.alarm = alarm;
+
+        // icon
+        Context context = GeoAlarms.context;
+        Resources res = context.getResources();
+        this.alarmIcon = BitmapFactory.decodeResource(res, R.drawable.app_icon);
 	}
 
 	@Override
 	public void draw(Canvas canvas, MapView mapView, boolean shadow) {
-		Projection projection = mapView.getProjection();
-
 		if (shadow == false) {
 			// get point
+            Projection projection = mapView.getProjection();
 			Point center = new Point();
+
+			// coordinates to convert
 			Coordinates coords = this.alarm.coordinates;
 			GeoPoint point = coords.toGeoPoint();
+
+			// put in `center` the coordinates in pixels
 			projection.toPixels(point, center);
+			
+			// draw radius
+			Paint circlePaint = new Paint();
+			circlePaint.setColor(0x30000000);
+			circlePaint.setStyle(Style.FILL_AND_STROKE);
+			// TODO: draw the circle calculating `radius`
+			canvas.drawCircle(center.x, center.y, 50, circlePaint);
+			
+			// draw alarm name
+			Paint textPaint = new Paint();
+			textPaint.setColor(Color.RED);
+			canvas.drawText(alarm.name, center.x, center.y, textPaint);
 
-			// paint 
-			Paint p = new Paint();
-			p.setColor(Color.RED);
-
-			canvas.drawCircle(center.x, center.y, 5, p);
-			//canvas.drawText("lat:" + (int) point.getLatitudeE6() + "long" + point.getLongitudeE6(), center.x + 10, center.y + 5, p);
+			// draw map pin
+		    canvas.drawBitmap(this.alarmIcon, 
+		                      center.x - alarmIcon.getWidth() / 2, 
+		                      center.y - alarmIcon.getHeight(), 
+		                      null);
+		
+			super.draw(canvas, mapView, shadow);
 		}
 	}
 }
